49609c85a1b2d6c5e318a7eb5dde3e02a5cfa021
Devendra Gaikwad <dev-gaikwad@users.noreply.github.com>
2023-07-08 22:00:16 +0530

    minor ui changes

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -6,12 +6,15 @@ const ReviewCard = ({ review }) => {
   const { pp, revName, rating, comment } = review;
   return (
     <article>
-      <div className={styles.header}>
-        <div className={styles.info}></div>
-        <img src={pp} alt='PP' />
-        <p>{revName}</p>
-        <div className={styles.rating}>
-          <p>{rating}</p>
+      <div className={styles.info}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <img src={pp} alt='PP' />
+            <p>{revName}</p>
+          </div>
+          <div className={styles.rating}>
+            <p>{rating}</p>
+          </div>
         </div>
         <p className={styles.comment}>{comment}</p>
       </div>
