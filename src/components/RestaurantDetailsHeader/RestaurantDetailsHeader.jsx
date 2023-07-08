a1b2b78462f28eaa5842ec571c9bde546c329803
Devendra Gaikwad <dev-gaikwad@users.noreply.github.com>
2023-07-08 21:51:40 +0530

    minor ui changes

--- src/components/RestaurantDetailsHeader/RestaurantDetailsHeader.jsx
+++ src/components/RestaurantDetailsHeader/RestaurantDetailsHeader.jsx
@@ -4,19 +4,22 @@ import styles from './RestaurantDetailsHeader.module.css';
 
 const RestaurantDetailsHeader = ({ details, setDisplayModal }) => {
   return (
-    <div>
-      <h1>{details.name}</h1>
-      <p>{details.description}</p>
-      <p>Phone : {details.phone}</p>
-      <ul>
-        <p>Our dishes:</p>
-        {details.menu.map((dish, index) => (
-          <li key={index}>{dish.name}</li>
-        ))}
-      </ul>
-      <p>Average Rating : {details.averageRating}</p>
-
-      <button onClick={() => setDisplayModal(true)}>Add Rating</button>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h1>{details.name}</h1>
+        <p>{details.description}</p>
+        <p>Phone : {details.phone}</p>
+        <ul>
+          <p>Our dishes:</p>
+          {details.menu.map((dish, index) => (
+            <li key={index}>{dish.name}</li>
+          ))}
+        </ul>
+        <p>Average Rating : {details.averageRating}</p>
+      </div>
+      <button className={styles.addBtn} onClick={() => setDisplayModal(true)}>
+        Add Review
+      </button>
     </div>
   );
 };
